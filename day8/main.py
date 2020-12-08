e64659cbf7ea01208cc6652c573ed11f95e63a85
Sam Little <kagutaba256@gmail.com>
2020-12-08 07:53:06 -0800

    brute force lmao

--- day8/main.py
+++ day8/main.py
@@ -1,5 +1,9 @@
 # sam little
 # aoc day 8
+
+import copy
+import time
+
 with open('day8-input.txt', 'r') as f:
     data = f.read()
 
@@ -19,9 +23,10 @@ def run(program):
     pc = 0
     acc = 0
     lastJump = 0
-    while pc < len(program):
-        if(instructions[pc]['hasRun'] == True):
-            break
+    t_end = time.time() + 0.10
+    while pc < len(program) and time.time() < t_end:
+        # if(instructions[pc]['hasRun'] == True):
+        #     break
         program[pc]['hasRun'] = True
 
         inst = program[pc]
@@ -41,7 +46,39 @@ def run(program):
             print('PANIC! unknown opcode')
             exit(1)
 
-    print(acc, lastJump)
+    if time.time() < t_end:
+        print(acc, lastJump)
+    else:
+        print('ran out of time')
+
+
+instructions_to_test = []
+for idx, item in enumerate(instructions):
+    p = {}
+    old = ''
+    new = ''
+    prog = copy.deepcopy(instructions)
+    if item['code'] == 'jmp':
+        prog[idx]['code'] = 'nop'
+        old = 'jmp'
+        new = 'nop'
+        p['desc'] = f'changed inst {idx} from {old} to {new}'
+        p['prog'] = prog
+        instructions_to_test.append(p)
+    elif item['code'] == 'nop' and item['num'] != 0:
+        prog[idx]['jmp'] = 'jmp'
+        old = 'nop'
+        new = 'jmp'
+        p['desc'] = f'changed inst {idx} from {old} to {new}'
+        p['prog'] = prog
+        instructions_to_test.append(p)
+    else:
+        pass
+
+while len(instructions_to_test) > 0:
+    prog = instructions_to_test.pop()
+    print(prog['desc'])
+    run(prog['prog'])
 
 
 run(instructions)
